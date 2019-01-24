7d334c09044c91db4af7e001303dc4c9814ad95a
Thomas Logan <thomaslilogan@gmail.com>
2019-01-24 14:12:13 -0600

    update agree command

--- com.collins.fmw.agree/command/src/com/collins/fmw/agree/command/PreferencesUtil.java
+++ com.collins.fmw.agree/command/src/com/collins/fmw/agree/command/PreferencesUtil.java
@@ -5,7 +5,6 @@ import java.io.InputStream;
 import java.nio.file.StandardCopyOption;
 
 import com.rockwellcollins.atc.agree.analysis.preferences.PreferenceConstants;
-import com.rockwellcollins.atc.z3.Z3Plugin;
 
 import jkind.JKindException;
 import jkind.SolverOption;
@@ -30,6 +29,74 @@ public class PreferencesUtil {
 		return solver;
 	}
 
+	public static String getJKindJar() {
+
+//		String bundleId = "com.collins.fmw.agree.command";
+//		Bundle bundle = Platform.getBundle(bundleId);
+//		URL url = bundle.getEntry("static/jkind.jar");
+		try {
+//			URL fileUrl = FileLocator.toFileURL(url);
+
+			File f = new File(PreferencesUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+			String targetDir = f.getParent();
+			InputStream input = PreferencesUtil.class.getResourceAsStream("/resources/jkind.jar");
+			File target = new File(targetDir, "jkind.jar");
+			java.nio.file.Files.copy(input, target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+
+			System.out.println("file path: " + target.toString());
+
+			String path = target.toString();
+			input.close();
+			return path;
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new JKindException("Unable to extract jkind.jar from plug-in", e);
+		}
+	}
+
+	private static String getArchDir() {
+		String name = System.getProperty("os.name").toLowerCase();
+		String arch = System.getProperty("os.arch").toLowerCase();
+
+		if (name.contains("windows")) {
+			if (arch.contains("64")) {
+				return "x64-win";
+			} else {
+				return "x86-win";
+			}
+		} else if (name.contains("mac os x")) {
+			return "x64-osx";
+		} else if (arch.contains("64")) {
+			return "x64-linux";
+		} else {
+			return "x86-linux";
+		}
+	}
+
+	private static String getExecutableName() {
+		boolean isWindows = System.getProperty("os.name").startsWith("Windows");
+		return isWindows ? "z3.exe" : "z3";
+	}
+
+	public static String getZ3Directory() {
+		try {
+
+			String archDir = getArchDir();
+			String exeName = getExecutableName();
+			InputStream input = PreferencesUtil.class.getResourceAsStream("/resources/" + archDir + "/" + exeName);
+
+			File f = new File(PreferencesUtil.class.getProtectionDomain().getCodeSource().getLocation().getPath());
+			String targetDir = f.getParent();
+			File target = new File(targetDir, exeName);
+			java.nio.file.Files.copy(input, target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			target.setExecutable(true);
+			return target.getParent();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new IllegalArgumentException("Unable to extract z3 from plug-in", e);
+		}
+	}
 
 	public static KindApi getConsistencyApi() {
 		KindApi api = getJKindApi();
@@ -46,7 +113,7 @@ public class PreferencesUtil {
 		JKindApi api = new JKindApi();
 		api.setJKindJar(getJKindJar());
 		try {
-			api.setEnvironment("Z3_HOME", Z3Plugin.getZ3Directory());
+			api.setEnvironment("Z3_HOME", getZ3Directory());
 		} catch (NoClassDefFoundError e) {
 			e.printStackTrace();
 			// Z3Plugin not present
@@ -81,7 +148,7 @@ public class PreferencesUtil {
 		JRealizabilityApi api = new JRealizabilityApi();
 		api.setJKindJar(getJKindJar());
 		try {
-			api.setEnvironment("Z3_HOME", Z3Plugin.getZ3Directory());
+			api.setEnvironment("Z3_HOME", getZ3Directory());
 		} catch (NoClassDefFoundError e) {
 			e.printStackTrace();
 			// Z3Plugin not present
@@ -93,32 +160,6 @@ public class PreferencesUtil {
 		return api;
 	}
 
-	public static String getJKindJar() {
 
-//		String bundleId = "com.collins.fmw.agree.command";
-//		Bundle bundle = Platform.getBundle(bundleId);
-//		URL url = bundle.getEntry("static/jkind.jar");
-		try {
-//			URL fileUrl = FileLocator.toFileURL(url);
-
-
-			InputStream input = PreferencesUtil.class.getResourceAsStream("/static/jkind.jar");
-			File target = new File("jkind.jar.tmp");
-			java.nio.file.Files.copy(input, target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
-			System.out.println("file path: " + target.toString());
-
-			String path = target.toString();
-			input.close();
-			return path;
-
-//			String jsonToHolPath = (FileLocator
-//					.toFileURL(FileLocator.find(bundle, new Path("static/jkind.jar"), null))).getFile();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new JKindException("Unable to extract jkind.jar from plug-in", e);
-		}
-	}
 
 }
