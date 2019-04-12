1ac651d58583832ed488beaa6fc727823c28b28b
Thomas Logan <thomaslilogan@gmail.com>
2019-04-12 18:44:04 -0500

    update

--- tools/agree/com.rockwellcollins.atc.agree.analysis/src/com/rockwellcollins/atc/agree/analysis/handlers/VerifySingleHandler.java
+++ tools/agree/com.rockwellcollins.atc.agree.analysis/src/com/rockwellcollins/atc/agree/analysis/handlers/VerifySingleHandler.java
@@ -1,21 +1,11 @@
 package com.rockwellcollins.atc.agree.analysis.handlers;
 
-import org.eclipse.jface.preference.IPreferenceStore;
-
-import com.rockwellcollins.atc.agree.analysis.Activator;
-import com.rockwellcollins.atc.agree.analysis.preferences.PreferenceConstants;
-
 public class VerifySingleHandler extends VerifyHandler {
 
 	public VerifySingleHandler() {
 		this.programType = ProgramType.Single;
-		IPreferenceStore prefs = Activator.getDefault().getPreferenceStore();
-		String solver = prefs.getString(PreferenceConstants.PREF_SOLVER);
-		if (solver != PreferenceConstants.SOLVER_Z3) {
-			throw new RuntimeException("You must select Z3 as your solver to check realizability.");
-		}
-	}
 
+	}
 
 	@Override
 	protected String getJobName() {
