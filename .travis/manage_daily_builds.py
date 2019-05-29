7f1cc4fc8f6b8ac3caa37e9de92d1846952ed62e
Karl Hoech <karl.hoech@collins.com>
2019-05-29 09:57:01 -0500

    Add pruning of stale product builds to release management

--- .travis/manage_daily_builds.py
+++ .travis/manage_daily_builds.py
@@ -1,9 +1,12 @@
+#!/usr/bin/env python3
+
 '''
 Created on May 16, 2019
 
 '''
 
 import os
+import re
 
 from github3 import GitHub
 from pprint import pformat
@@ -14,6 +17,8 @@ AUTH_TOKEN = os.environ['GH_TOKEN'] if 'GH_TOKEN' in os.environ.keys() else None
 
 REPOSITORY_OWNER = 'loonwerks'
 REPOSITORY_REPO = 'formal-methods-workbench'
+
+PRODUCT_ASSET_PATTERN = re.compile(r'com.collins.fmw.ide-\d+\.\d+\.\d+-(\d{12})-.*')
 
 def manage_daily_builds():
     # obtain git handle
@@ -38,6 +43,24 @@ def manage_daily_builds():
             if rel_tag_ref is not None:
                 print('  deleting tag %s' % (rel_tag_ref.ref))
                 rel_tag_ref.delete()
+        else:
+            # Look for stale files in the release
+            assets = rel.assets()
+            print('In release %s found assets:' % (rel.name))
+            for asset in assets:
+                match = PRODUCT_ASSET_PATTERN.search(asset.name)
+                print('  asset named %s matches %s' % (asset.name, match.group(1) if match is not None else 'None'))
+            build_times = sorted([PRODUCT_ASSET_PATTERN.search(x.name).group(1) for x in assets if PRODUCT_ASSET_PATTERN.search(x.name)])
+            latest_build_time = build_times[-1] if build_times else None
+            print('Lastest build time is %s' % (latest_build_time))
+            for asset in assets:
+                match = PRODUCT_ASSET_PATTERN.search(asset.name)
+                # print('  asset named %s matches %s' % (asset.name, match.group(1) if match is not None else 'None'))
+                if match is not None:
+                    asset_build_time = match.group(1)
+                    if asset_build_time != latest_build_time:
+                        print('deleting stale asset %s' % (asset.name))
+                        asset.delete()
 
 if __name__ == '__main__':
     manage_daily_builds()
