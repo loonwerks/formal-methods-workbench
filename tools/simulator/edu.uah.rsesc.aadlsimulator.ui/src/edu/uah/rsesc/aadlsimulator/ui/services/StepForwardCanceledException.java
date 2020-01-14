4012157437a41d22fef1050e9715afde57a9758c
Karl Hoech <karl.hoech@collins.com>
2020-01-14 08:00:34 -0600

    Move AGREE simulator to https://github.com/loonwerks/AGREE.git

--- tools/simulator/edu.uah.rsesc.aadlsimulator.ui/src/edu/uah/rsesc/aadlsimulator/ui/services/StepForwardCanceledException.java
+++
@@ -1,8 +0,0 @@
-package edu.uah.rsesc.aadlsimulator.ui.services;
-
-@SuppressWarnings("serial")
-public class StepForwardCanceledException extends Exception {
-	public StepForwardCanceledException(final Throwable cause) {
-		super(cause.getMessage(), cause);
-	}
-}
